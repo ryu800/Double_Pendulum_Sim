5f2077b9fa2b11a6ad522ea6b244936bae915233
Mason <mason@ajmani.org>
2026-07-27 14:21:56 -0400

    added step function, angle history, variables update per step, and the frame work for the visuals

--- double_pendulum_sim.py
+++ double_pendulum_sim.py
@@ -21,23 +21,53 @@ av2 = 0 #rad/s
 
 
 
-def acceleration():
-    denom=(m2*(l1**2)*(l2**2))(m1+m2)-m2*(np.cos(theta1-theta2)**2)
+def accelerations(theta1,theta2,av1,av2):
     delta=theta1-theta2
+    denom=(m2*(l1**2)*(l2**2))*((m1+m2)-m2*(np.cos(delta)**2))
+    
 
     alpha1 = (
         (m2*l1*(l2**2))*(-m2*l2*(av2**2)*(np.sin(delta))-(m1+m2)*g*(np.sin(theta1))-m2*l1*(av1**2)*(np.sin(delta))*(np.cos(delta))+m2*g*(np.sin(theta2))*(np.cos(delta))) / denom
     )
 
     alpha2 = (
-        (m2*(l1**2)*l2)((m1_m2)*l1*(av1**2)*(np.sin(delta))-(m1+m2)*g*(np.sin(theta2))+m2*l2*(av2**2)*(np.cos(delta))*(np.sin(delta))+(m1+m2)*g*(np.cos(delta))*(np.sin(theta1))) / denom
+        (m2*(l1**2)*l2)*((m1+m2)*l1*(av1**2)*(np.sin(delta))-(m1+m2)*g*(np.sin(theta2))+m2*l2*(av2**2)*(np.cos(delta))*(np.sin(delta))+(m1+m2)*g*(np.cos(delta))*(np.sin(theta1))) / denom
     )
+
     return alpha1, alpha2
 
-#   ani = animation.FuncAnimation(fig=, func=, frames=, interval=)
+def step(theta1,theta2,av1,av2,dt): #euler integration!! -will use RK4 in future :)
+    alpha1, alpha2 = accelerations(theta1,theta2,av1,av2)
+
+    theta1_new=theta1 + av1*dt
+    theta2_new=theta2 + av2*dt
+    av1_new=av1 + alpha1*dt
+    av2_new=av2 +alpha2*dt
+
+    return theta1_new, theta2_new, av1_new, av2_new
+
+theta1_history=np.zeros(n_steps) #creates an empty array the size of the number of frames, will store each angle as the sim goes through each step
+theta2_history=np.zeros(n_steps)
+
+for i in range(n_steps): # updating values every frame.
+    theta1_history[i] = theta1 #stores angle in coordsponding frame in the array
+    theta2_history[i] = theta2
+    theta1, theta2, av1, av2 = step(theta1,theta2,av1,av2,dt) #updates values each step/frame
+
+#convert angles in positions (x1,y1) (x2,y2)
+x1 = l1*(np.sin(theta1))
+y1 = -l1*(np.cos(theta1))
+
+x2 = l1*(np.sin(theta1))+l2*(np.sin(theta2))
+y2 = -l1*(np.cos(theta1))-l2*(np.cos(theta2))
+
+#setting up visuals
+
+fig, ax = plt.subplots(figsize=(8,8)) 
+ax.set_xlim(-(l1+l2+1),l1+l2+1) #domain is 1 unit greater the max arms length on either side
+ax.set_ylim(-(l1+l2+1),l1+l2+1)
+ax.set_aspect('equal')
 
 plt.show()
 
 
-
-print(np.cos(np.pi)**2)
